442291bd5f8ca1e17b980286baf64f6c0399b7e5
Julian <julian.57@outlook.fr>
2022-09-10 20:46:06 +0200

    add the default home page template and controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class DashboardController extends AbstractController
+class HomeController extends AbstractController
 {
-    #[Route('/dashboard', name: 'app_dashboard')]
+    #[Route('/home', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('dashboard.html.twig', [
-            'controller_name' => 'DashboardController',
+        return $this->render('home/home_default.html.twig', [
+            'controller_name' => 'HomeController',
         ]);
     }
 }
