2b7c5736b0e3f7aac1b5b52713ea3c377424d4ee
Julian <julian.57@outlook.fr>
2022-10-01 18:01:22 +0200

    test permission with is granted

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,9 +10,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 class HomeController extends AbstractController
 {
     /* Fonction qui va nous rediriger vers le template twig de la page d'accueil */
-    #[IsGranted('ROLE_ADMIN')]
-    #[IsGranted('ROLE_FRANCHISE')]
-    #[IsGranted('ROLE_STRUCTURE')]
+    #[IsGranted('ROLE_ADMIN' || 'ROLE_FRANCHISE' || 'ROLE_STRUCTURE')]
     #[Route('/home', name: 'app_home')]
     public function index(): Response
     {
