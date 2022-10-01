60b5b59868e320cfedd7f0059c4fa704600c9df0
Julian <julian.57@outlook.fr>
2022-10-01 17:49:32 +0200

    Correction home controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-=
+
 class HomeController extends AbstractController
 {
     /* Fonction qui va nous rediriger vers le template twig de la page d'accueil */
