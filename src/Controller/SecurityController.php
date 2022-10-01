b9f9c9483aa93d4e37c034c0c9a34273588fe8cf
Julian <julian.57@outlook.fr>
2022-10-01 17:38:28 +0200

    change my login IsGranted

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,9 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
-#[IsGranted('ROLE_USER')]
 class SecurityController extends AbstractController
 {
     #[Route(path: '/', name: 'app_login')]
